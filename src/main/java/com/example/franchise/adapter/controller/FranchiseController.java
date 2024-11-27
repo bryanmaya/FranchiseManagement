f91b47d54d23e3deaf7beb2d9d780578a364806a
brayamcastrillon <brayamcastrillon@subocol.com>
2024-11-27 00:38:13 -0500

    feature/7_producto_con_mas_stock_sucursal_franquicia_bcm feat: Se crea nueva funcionalidad para consultar el producto con mas stock en cada sucursal por una franquicia especifica

--- src/main/java/com/example/franchise/adapter/controller/FranchiseController.java
+++ src/main/java/com/example/franchise/adapter/controller/FranchiseController.java
@@ -2,7 +2,9 @@ package com.example.franchise.adapter.controller;
 
 import com.example.franchise.adapter.mapper.FranchiseMapper;
 import com.example.franchise.application.dto.FranchiseDTO;
+import com.example.franchise.application.dto.TopProductDTO;
 import com.example.franchise.domain.service.FranchiseService;
+import com.example.franchise.domain.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ public class FranchiseController {
 
     private final FranchiseService franchiseService;
     private final FranchiseMapper franchiseMapper;
+    private final ProductService productService;
 
     @PostMapping
     public ResponseEntity<FranchiseDTO> addFranchise(@RequestBody FranchiseDTO franchiseDTO) {
@@ -41,5 +44,11 @@ public class FranchiseController {
                     return ResponseEntity.ok(franchiseMapper.toDTO(franchiseService.save(franchise)));
                 })
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}/top-products")
+    public ResponseEntity<List<TopProductDTO>> getTopProductsByBranch(@PathVariable Long id) {
+        List<TopProductDTO> topProducts = productService.getTopProductsByBranch(id);
+        return ResponseEntity.ok(topProducts);
     }
 }
--- src/main/java/com/example/franchise/domain/service/ProductService.java
+++ src/main/java/com/example/franchise/domain/service/ProductService.java
@@ -1,12 +1,18 @@
 package com.example.franchise.domain.service;
 
+import com.example.franchise.application.dto.TopProductDTO;
 import com.example.franchise.domain.model.Branch;
+import com.example.franchise.domain.model.Franchise;
 import com.example.franchise.domain.model.Product;
 import com.example.franchise.domain.port.BranchPort;
+import com.example.franchise.domain.port.FranchisePort;
 import com.example.franchise.domain.port.ProductPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,6 +22,8 @@ public class ProductService {
     private final ProductPort productPort;
 
     private final BranchPort branchPort;
+
+    private final FranchisePort franchisePort;
 
     public Product save(Product product) {
         return productPort.save(product);
@@ -32,5 +40,25 @@ public class ProductService {
         branch.getProducts().remove(product);
         branchPort.save(branch);
         productPort.deleteById(id);
+    }
+
+    public List<TopProductDTO> getTopProductsByBranch(Long franchiseId) {
+        Optional<Franchise> franchiseOptional = franchisePort.findById(franchiseId);
+
+        if (franchiseOptional.isEmpty()) {
+            throw new IllegalArgumentException("Franchise not found with id: " + franchiseId);
+        }
+
+        Franchise franchise = franchiseOptional.get();
+        List<TopProductDTO> topProducts = new ArrayList<>();
+
+        for (Branch branch : franchise.getBranches()) {
+            branch.getProducts().stream()
+                    .max(Comparator.comparingInt(Product::getStock)).ifPresent(topProduct ->
+                            topProducts.add(new TopProductDTO(branch.getName(), topProduct.getName(), topProduct.getStock())));
+
+        }
+
+        return topProducts;
     }
 }
